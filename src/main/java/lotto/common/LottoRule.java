7f83a6f03c12426254cd702b455366ae8995cf12
gyeomcode <gyeomcode@gmail.com>
2025-11-02 16:49:24 +0900

    refactor: final 키워드를 추가한다

--- src/main/java/lotto/common/LottoRule.java
+++ src/main/java/lotto/common/LottoRule.java
@@ -2,8 +2,8 @@ package lotto.common;
 
 public class LottoRule {
 
-    public static int LOTTO_PRICE_UNIT = 1000;
-    public static int LOTTO_NUMBERS_LENGTH = 6;
-    public static int LOTTO_NUMBER_MIN_VALUE = 1;
-    public static int LOTTO_NUMBER_MAX_VALUE = 45;
+    public static final int LOTTO_PRICE_UNIT = 1000;
+    public static final int LOTTO_NUMBERS_LENGTH = 6;
+    public static final int LOTTO_NUMBER_MIN_VALUE = 1;
+    public static final int LOTTO_NUMBER_MAX_VALUE = 45;
 }
